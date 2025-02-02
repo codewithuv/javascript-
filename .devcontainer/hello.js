b2ec72de3acae4e44e1f6a79d7ae5fbb1811ba24
Utkarsh Gupta <132091674+codewithuv@users.noreply.github.com>
2025-02-02 19:22:09 +0000

    string

--- .devcontainer/hello.js
+++ .devcontainer/hello.js
@@ -4,3 +4,7 @@ console.log(typeof(score))
 let numberInString =Number(score)
 console.log(typeof(numberInString))
 console.log(numberInString)
+
+let name ="utkarsh is my name"
+console.log(name[2])
+console.log(name.substring(0,8).toUpperCase())
